fe6e13fd804dcdaf0f551db117ed77eb20a49760
Kyle Anthony Nierras <20-1-01205@vsu.edu.ph>
2024-03-23 18:18:28 +0800

    Update

--- src/components/common/item_render.vue
+++ src/components/common/item_render.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="w-full h-full grid sm:grid-cols-1 md:grid-cols-3 gap-5 mt-5">
         <div v-for="(item, index) in Items" :key="index" class="h-auto m-2">
-            <a href="#">
+            <router-link :to="`/product/${item.id}`" @click="goToProduct(item.id)">
                 <img class="rounded-lg h-80 w-auto object-cover" :src="item.url" alt="" />
-            </a>
+            </router-link>
             <div class="p-3">
                 <div class="flex flex-col justify-between font-bold gap-1">
                     <p class="font-bold text-2xl">{{ item.name }}</p>
@@ -17,6 +17,10 @@
 
 <script setup lang="ts">
 import { Items as Item } from '../../lib/features/user_dashboard/models/item';
+
+function goToProduct(id: number) {
+  localStorage.setItem('selectedProduct', id.toString());
+}
 
 defineProps({
     Items: {
